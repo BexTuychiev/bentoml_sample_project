d8b1669ff3f96a0a039ae45ec78b8243ba6e8f24
BexTuychiev <bex@ibexprogramming.com>
2022-07-27 14:11:10 +0500

    data: Generate a sample dataset

--- src/generate_data.py
+++ src/generate_data.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 n_samples = 100000
 n_features = 10
+seed = 42
 
 # Generate the data
-X, y = make_classification(n_samples=n_samples, n_features=n_features)
+X, y = make_classification(n_samples=n_samples, n_features=n_features, random_state=seed)
 
 col_names = [f'feature_{i}' for i in range(n_features)] + ['target']
 
@@ -13,3 +14,6 @@ df = pd.DataFrame(data=X)
 df['target'] = y
 
 df.columns = col_names
+
+# Save
+df.to_csv('data/data.csv', index=False)
