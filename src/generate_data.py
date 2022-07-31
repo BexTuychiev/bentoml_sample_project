e0c946ca20d513a03d24eb8230fbdc616242b8e2
BexTuychiev <bex@ibexprogramming.com>
2022-07-31 16:05:20 +0500

    feature: Create a function to generate a synthetic dataset

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,19 +1,24 @@
 from sklearn.datasets import make_classification
 import pandas as pd
 
-n_samples = 100000
-n_features = 10
-seed = 42
 
-# Generate the data
-X, y = make_classification(n_samples=n_samples, n_features=n_features, random_state=seed)
+def generate_data(n_samples, n_features, n_informative, path):
+    """
+    A simple function to save a synthetic dataset to path.
+    """
+    # Generate the data
+    X, y = make_classification(n_samples=n_samples, n_features=n_features,
+                               n_informative=n_informative)
 
-col_names = [f'feature_{i}' for i in range(n_features)] + ['target']
+    # Save it as a CSV
+    feature_names = [f"feature_{i}" for i in range(n_features)]
 
-df = pd.DataFrame(data=X)
-df['target'] = y
+    df = pd.DataFrame(X, columns=feature_names)
+    df["target"] = y
 
-df.columns = col_names
+    df.to_csv(path, index=False)
 
-# Save
-df.to_csv('data/data.csv', index=False)
+
+if __name__ == "__main__":
+    n_samples, n_features = 10000, 7
+    generate_data(n_samples, n_features, 5, "data/data.csv")
