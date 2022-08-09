27224c2e850102efcbbc9e5e3a0784acbfd6a206
BexTuychiev <bex@ibexprogramming.com>
2022-08-09 16:52:56 +0500

    feature: Change the API of XGBoost from Sklearn to Learning

--- src/train.py
+++ src/train.py
@@ -1,19 +1,25 @@
 import xgboost as xgb
 import pandas as pd
 import bentoml
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def train_xgb_save(X, y, tag_name="xgb_final"):
     """
     A simple function to train a model and save it to BentoML model store.
     """
-    # Initialize a classifier
-    clf = xgb.XGBClassifier(tree_method="gpu_hist")
+    # Create DMatrix
+    dtrain = xgb.DMatrix(X, label=y)
+    # Specify parameters for a binary classification problem
+    params = {"objective": "binary:logistic", "booster": "gbtree",
+              "tree_method": "gpu_hist", "eval_metric": "auc"}
 
-    # Train and save
-    clf.fit(X, y)
+    # Train
+    booster = xgb.train(params, dtrain, num_boost_round=20)
 
-    bentoml.sklearn.save_model(tag_name, clf)
+    bentoml.xgboost.save_model(tag_name, booster)
 
 
 if __name__ == "__main__":
@@ -22,4 +28,4 @@ if __name__ == "__main__":
     X, y = data.drop("target", axis=1), data[["target"]]
 
     # Train and save
-    train_xgb_save(X, y)
+    train_xgb_save(X, y, "xgb_booster")
